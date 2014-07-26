90e82c206d1ad30141e55105ef72892f2e4111e1
Shawn M Moore <shawn.moore@iinteractive.com>
2014-07-26 17:37:32 -0400

    Display current video in queue window

--- PMC/PMCQueueViewController.m
+++ PMC/PMCQueueViewController.m
@@ -5,6 +5,7 @@
 
 @property (nonatomic, strong) NSURLSession *session;
 @property (nonatomic, strong) NSArray *videos;
+@property (nonatomic, strong) NSDictionary *currentVideo;
 
 @end
 
@@ -17,9 +18,6 @@
         NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
         NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
         self.session = session;
-
-        [self.refreshControl beginRefreshing];
-        [self refreshVideos:self.refreshControl];
     }
     return self;
 }
@@ -31,40 +29,77 @@
     [self.refreshControl addTarget:self action:@selector(refreshVideos:) forControlEvents:UIControlEventValueChanged];
 
     [self.tableView registerNib:[UINib nibWithNibName:@"PMCVideoTableViewCell" bundle:nil] forCellReuseIdentifier:@"Video"];
-}
 
--(void)setVideos:(NSArray *)videos {
-    _videos = videos;
-    [self.tableView reloadData];
+    [self.refreshControl beginRefreshing];
+    [self refreshVideos:self.refreshControl];
 }
 
 -(void)refreshVideos:(UIRefreshControl *)sender {
-    NSURLSessionTask *task = [self.session dataTaskWithURL:[NSURL URLWithString:@"http://10.0.1.13:5000/queue"] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+    NSURLSessionTask *queueTask = [self.session dataTaskWithURL:[NSURL URLWithString:@"http://10.0.1.13:5000/queue"] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         NSArray *videos = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
 
         dispatch_async(dispatch_get_main_queue(), ^{
             self.videos = videos;
-            [sender endRefreshing];
+
+            if (self.currentVideo) {
+                [self.tableView reloadData];
+                [sender endRefreshing];
+            }
         });
     }];
-    [task resume];
+
+    NSURLSessionTask *currentTask = [self.session dataTaskWithURL:[NSURL URLWithString:@"http://10.0.1.13:5000/current"] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSDictionary *currentVideo = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.currentVideo = currentVideo;
+
+            if (self.videos) {
+                [self.tableView reloadData];
+                [sender endRefreshing];
+            }
+        });
+    }];
+
+    [queueTask resume];
+    [currentTask resume];
 }
 
 #pragma mark - UITableViewDataSource
 
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.videos.count;
+    if (section == 0) {
+        return 1;
+    }
+    else {
+        return self.videos.count;
+    }
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSDictionary *video = self.videos[indexPath.row];
     PMCVideoTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Video" forIndexPath:indexPath];
+    NSDictionary *video;
+    BOOL isCurrent = NO;
+
+    if (indexPath.section == 0) {
+        video = self.currentVideo;
+        isCurrent = YES;
+    }
+    else {
+        video = self.videos[indexPath.row];
+    }
 
     id label = [video valueForKeyPath:@"label.ja"];
     if (!label || label == [NSNull null]) {
         label = [video valueForKeyPath:@"label.en"];
     }
+
     cell.titleLabel.text = label;
+    cell.backgroundColor = isCurrent ? [UIColor colorWithWhite:0.95f alpha:1] : [UIColor whiteColor];
 
     id identifier = [video valueForKeyPath:@"identifier"];
     if (!identifier || identifier == [NSNull null]) {
