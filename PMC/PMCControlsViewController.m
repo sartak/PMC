f26f36a2905fe63a80843c849713a0a385071e36
Shawn M Moore <shawn.moore@iinteractive.com>
2014-08-09 16:42:45 -0400

    Add shutdown pi button

--- PMC/PMCControlsViewController.m
+++ PMC/PMCControlsViewController.m
@@ -19,17 +19,24 @@
     return self;
 }
 
--(void)sendMethod:(NSString *)method {
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://10.0.1.13:5000/current"]];
+-(void)sendMethod:(NSString *)method toEndpoint:(NSString *)endpoint {
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://10.0.1.13:5000/%@", endpoint]];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     request.HTTPMethod = method;
     NSURLSessionTask *task = [self.session dataTaskWithRequest:request];
     [task resume];
 }
 
--(IBAction)nextVideo   { [self sendMethod:@"DELETE"]; }
--(IBAction)nextAudio   { [self sendMethod:@"NEXTAUDIO"]; }
--(IBAction)nextSubs    { [self sendMethod:@"NEXTSUBS"]; }
--(IBAction)playPause   { [self sendMethod:@"PLAYPAUSE"]; }
--(IBAction)stopPlaying { [self sendMethod:@"STOP"]; }
+-(void)sendMethodToCurrent:(NSString *)method {
+    [self sendMethod:method toEndpoint:@"current"];
+}
+
+-(IBAction)nextVideo   { [self sendMethodToCurrent:@"DELETE"]; }
+-(IBAction)nextAudio   { [self sendMethodToCurrent:@"NEXTAUDIO"]; }
+-(IBAction)nextSubs    { [self sendMethodToCurrent:@"NEXTSUBS"]; }
+-(IBAction)playPause   { [self sendMethodToCurrent:@"PLAYPAUSE"]; }
+-(IBAction)stopPlaying { [self sendMethodToCurrent:@"STOP"]; }
+
+-(IBAction)shutdownPi   { [self sendMethod:@"SHUTDOWN" toEndpoint:@"pi"]; }
 
 @end
